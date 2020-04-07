b3827817e487e1917b3cce9802b1362deb0657c4
liyu10000 <yuli00986081@gmail.com>
2020-04-07 10:56:50 -0400

    add parse funcs for cameras.txt and images.txt

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ import os
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+from pprint import pprint
 from mpl_toolkits.mplot3d import Axes3D
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 
@@ -19,6 +20,9 @@ def get_frame(video_name, frame_dir):
 
 
 def parse_points3D(txt):
+    """ Parse points3D.txt from sparse reconstruction.
+        Return a numpy array of xyz coordinates, (n,3).
+    """
     xyz = []
     with open(txt, 'r') as f:
         for line in f.readlines():
@@ -32,10 +36,49 @@ def parse_points3D(txt):
     return np.array(xyz)
 
 def parse_cameras(txt):
-    pass
+    """ Parse cameras.txt from sparse reconstruction.
+        Assume a single camera model:
+            SIMPLE_RADIAL params: f, cx, cy, k.
+        Return a python dict containing camara params.
+    """
+    cameras = {}
+    with open(txt, 'r') as f:
+        for line in f.readlines():
+            if line.startswith('#'):
+                continue
+            tokens = line.strip().split()
+            cameras['CAMERA_ID'] = int(tokens[0])
+            cameras['MODEL'] = tokens[1]
+            cameras['WIDTH'] = int(tokens[2])
+            cameras['HEIGHT'] = int(tokens[3])
+            cameras['PARAMS'] = [float(tokens[i]) for i in range(4, len(tokens))]
+    return cameras
 
 def parse_images(txt):
-    pass
+    """ Parse images.txt from sparse reconstruction.
+        Return a python dict containing camera pose for each image.
+    """
+    keys = ['IMAGE_ID', 'QW', 'QX', 'QY', 'QZ', 'TX', 'TY', 'TZ', 'CAMERA_ID', 'NAME']
+    images = {key:[] for key in keys}
+    readline = False
+    with open(txt, 'r') as f:
+        for line in f.readlines():
+            if line.startswith('#') or not readline:
+                readline = True
+            else:
+                readline = False
+                tokens = line.strip().split()
+                images['IMAGE_ID'].append(int(tokens[0]))
+                images['QW'].append(float(tokens[1]))
+                images['QX'].append(float(tokens[2]))
+                images['QY'].append(float(tokens[3]))
+                images['QZ'].append(float(tokens[4]))
+                images['TX'].append(float(tokens[5]))
+                images['TY'].append(float(tokens[6]))
+                images['TZ'].append(float(tokens[7]))
+                images['CAMERA_ID'].append(int(tokens[8]))
+                images['NAME'].append(tokens[9])
+    return images
 
 
 def detect_outliers_1d(data, threshold):
@@ -153,7 +196,15 @@ def plot3D(inplane_points, outplane_points, plot_plane=False, model=None, plot_b
 
 
 if __name__ == '__main__':
-    video_name = './home/home.MOV'
-    frame_dir = './home/frames'
-    get_frame(video_name, frame_dir)
+    # video_name = './home/home.MOV'
+    # frame_dir = './home/frames'
+    # get_frame(video_name, frame_dir)
 
+    # cameras_txt = './home/cameras.txt'
+    # cameras = parse_cameras(cameras_txt)
+    # print(cameras)
+
+    images_txt = './home/images.txt'
+    images = parse_images(images_txt)
+    print(images)
+    pprint({k:len(v) for k,v in images.items()})
